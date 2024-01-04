d043b654001434cb1cbaa2fcc68096e4e0845f19
SSK443 <143587320+SSK443@users.noreply.github.com>
2024-01-04 15:00:19 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,87 @@
+// for(i=1;i<=10;i++){
+//   console.log('hello world',i)
+// }
+// console.log('out',i)
+
+// for(i=1;i<=10;i++){
+//   console.log(i)
+// }
+// for(i=10;i>=1;i--){
+//   console.log(i)
+// }
+
+
+// for(i=1;i<=8;i++){
+//   if(i%2==0){
+//     console.log(i)
+//   }
+// }
+// for(i=10;i<=30;i++){
+//   if(i%2==0){
+//     console.log(i)
+//   }
+// }
+// for(i=1;i<=100;i++){
+//   if(i%2==0&&i%5==0){
+//     console.log(i)
+//   }
+// }
+// let n1=3;
+// let n2=15;
+// for(;n1<=n2;n1++){
+//   if(n1%2!==0){
+//     console.log(n1**2)
+//   }
+// }
+// let n=5;
+// let sum=0;
+// for(i=1;i<=n;i++){
+//   sum+=i;
+// }
+// console.log(sum);
+// let n=1;
+// let sum=0;
+// for(i=5;i>=n;i--){
+//   sum+=i;
+// }
+// console.log(sum);
+// let n=7;
+// let fact=1;
+// for(i=1;i<=n;i++){
+//   fact*=i;
+// }
+// console.log(fact);
+// let r1=1;
+// let r2=10;
+// let sum=0;
+// for(;r1<=r2;r1++){
+//   if(r1%2==0){
+//     sum+=r1;
+//   }
+  
+
+// }
+// console.log(sum)
+
+// let n=10;
+// for(i=1;i<=n;i++){
+//   if(n%i==0){
+//     console.log(i)
+//   }
+// }
+
+// for(let i=1;i>0;i++){
+//   console.log('i')}
+// let n=10;
+let n=12;
+if(n%2==0){
+  console.log('not prime')
+}else{
+console.log('prime')
+}
+
+
+
+    
+    
+  
